c62a3a376358ee5a9a4a3a26f541630ba3e08248
karl-maker <karljohanbailey98@gmail.com>
2022-02-06 13:18:45 -0400

    Change Test

--- components/example/index.js
+++ components/example/index.js
@@ -5,8 +5,6 @@ import { useState, useEffect } from "react";
 export default function ExampleComponent({ course, children }) {
   const [count, setCount] = useState(0);
 
-  // Fetch? ASYNC AWAIT
-
   useEffect(() => {
     //...
     // api/example ===> Hello World
