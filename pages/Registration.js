acb9207514d9061e449c9774ebae28260c7823ea
Leandre Fontenelle <72582917+Lee733@users.noreply.github.com>
2022-04-14 18:22:37 -0400

    Registration page

---
+++ pages/Registration.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './index.css';
+
+
+function SignUp(){
+    
+    return(
+        //Div for sign up form.
+        <div className='SignUpForm'> 
+            <form> 
+                <div className='PersonalInfo'>
+                    <h1>Sign Up</h1>
+                        <label>
+                                <input className='textbox' type="text" placeholder='First Name'/>
+                            </label>
+                        <br/>
+                            <label>
+                                <input className='textbox' type="text" placeholder='Last Name'/>
+                            </label>
+                        <br/>
+                            <label>
+                                <input className='textbox' type="text" placeholder='Student ID Number'/>
+                            </label>
+                        <br/>
+                            <label>
+                                <input className='textbox' type="text" placeholder='Student Email'/>
+                            </label>
+                        <br/>
+                            <label>
+                                <input className='textbox' type="text" placeholder='Password'/>
+                            </label>
+                        <br/>
+                            <button className= 'RegisterBtn'>
+                                Register
+                            </button>
+                    </div>
+                <br/>
+            </form>
+        </div>
+    )
+
+}
+
+export default SignUp;
